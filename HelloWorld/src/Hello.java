c10e4c85540f952b57c4493083b9145ee0e433bb
new_dragon <tkddyd1126@gmail.com>
2023-12-04 00:11:01 +0900

    init

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        double data = 20.00;
+        System.out.println(data);
+        System.out.print("Hello, Tim");
+        System.out.print("Hello World");
+    }
+}
